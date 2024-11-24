bf5d3851739c57f90677dd1d13030d0e5a36797a
Kang Shend Rong <shendrongddv@gmail.com>
2024-11-24 14:45:48 +0700

    Section Hero

--- components/site/mobile-menu.tsx
+++ components/site/mobile-menu.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetDescription,
   SheetFooter,
